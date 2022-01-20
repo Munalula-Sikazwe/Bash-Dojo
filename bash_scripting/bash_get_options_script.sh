d7acbfe31680c3fb355c43524b96d5522d4a7f64
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2022-01-20 15:44:07 +0200

    calculated the fahreheit value.

--- bash_scripting/bash_get_options_script.sh
+++ bash_scripting/bash_get_options_script.sh
@@ -2,7 +2,8 @@
 
 while getopts "f:c:" temperature; do
 case "$temperature" in 
-    f)'';;
-    c)'';;
+    f)result=$(echo "scale=2; (($OPTARG-32)%(9/5))" | bc);;
+    c)result=$(echo "scale=2; ($OPTARG * (9/5)) + 32" | bc );;
 esac
+
 done
