b7313716125c81dd772eabba620716798539028b
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2022-01-20 15:56:37 +0200

    added some minor error handling.

--- bash_scripting/bash_get_options_script.sh
+++ bash_scripting/bash_get_options_script.sh
@@ -2,8 +2,9 @@
 
 while getopts "f:c:" temperature; do
 case "$temperature" in 
-    f)result=$(echo "scale=2; (($OPTARG-32)%(9/5))" | bc);;
+    f)result=$(echo "scale=2; (($OPTARG-32)/(9/5))" | bc);;
     c)result=$(echo "scale=2; ($OPTARG * (9/5)) + 32" | bc );;
+    \?);;
 esac
 
 done
