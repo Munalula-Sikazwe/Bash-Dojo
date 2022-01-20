af4d5cbdd130bdff6c4b553f56094ae9be778c50
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2022-01-20 16:01:56 +0200

    tested the error handling.

--- bash_scripting/bash_get_options_script.sh
+++ bash_scripting/bash_get_options_script.sh
@@ -4,7 +4,7 @@ while getopts "f:c:" temperature; do
 case "$temperature" in 
     f)result=$(echo "scale=2; (($OPTARG-32)/(9/5))" | bc);;
     c)result=$(echo "scale=2; ($OPTARG * (9/5)) + 32" | bc );;
-    \?);;
+    \?)result="Error: Please enter a valid option.";;
 esac
 
 done
